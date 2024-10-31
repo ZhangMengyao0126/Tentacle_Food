dd69ec586c821a491a7308f205fb88cce479a897
ZhangMengyao <377922802@qq.com>
2024-10-31 21:02:34 +0200

    Add annotations

--- Assets/Scripts/Interaction_System/IInteractable.cs
+++ Assets/Scripts/Interaction_System/IInteractable.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace VHS
+//Namespace: A collection of name definitions of classes,interfaces, methods, and variables.
 {
     public interface IInteractable
+    //Interface: A contract that specifies a set of rules or a protocol that classes must follow if they choose to implement that interface. 
     {
         float HoldDuration { get; }
+        //{ get; }:A get accessor, meaning it's a read-only property.
 
         bool HoldInteract { get; }
         bool MultipleUse { get; }
--- Assets/Scripts/Scriptable_Objects/InteractionInputData.cs
+++ Assets/Scripts/Scriptable_Objects/InteractionInputData.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace VHS
 {
     [CreateAssetMenu(fileName = "InteractionInputData", menuName = "InteractionSystem/InputData")]
+    //CreateAssetMenu: Create instances of your ScriptableObject directly from the Unity Editor.
+    //fileName: The defalt name of the new ScriptableObject.
+    //menuName: Where in the Create Asset menu this option will appear in the Unity Editor(Create ¡ú InteractionSystem ¡ú InputData).
     public class InteractionInputData : ScriptableObject
+    //":":
+    //1. Class Inheritance:
+    /////When a class is followed by a : and another class name, it means the class inherits from the specified base class.
+    /////This means it will inherit all the methods and properties of the base class, allowing it to reuse and potentially override those methods.
+    //2. Interface Implementation:
+    /////When a class is followed by : and an interface name, it means the class implements the specified interface.
+    /////This requires the class to define all methods and properties specified in the interface, 
+    /////following a ¡°contract¡± of required methods and properties.
+    //ScriptableObject: A class that allows you to create data containers that can be saved as assets in your project.
     {
         private bool m_interactedClicked;
         private bool m_interactedRelease;
